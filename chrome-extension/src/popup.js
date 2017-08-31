4cda3baee3e538cfd03a70665bf054ce03534ae8
Jonathan MASSUCHETTI <jonathan.massuchetti@boursorama.fr>
2017-08-31 16:37:47 +0200

    clean popup

--- chrome-extension/src/popup.js
+++ chrome-extension/src/popup.js
@@ -1,30 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let status = document.getElementById("status");
 
+    let status = document.getElementById("status");
     status.textContent = "LOADED";
 
-    document.getElementById("addTab").addEventListener('click', function(){
-        status.textContent = "addTab CLICKED";
-        chrome.runtime.sendMessage({ds: "addTab"});
-    });
-
-    document.getElementById("createWindows").addEventListener('click', function(){
-        status.textContent = "LOAD CLICKED";
-        chrome.runtime.sendMessage({wm: "createWindows"});
-    });
-
-    document.getElementById("closeWindows").addEventListener('click', function() {
-        status.textContent = "CLOSE CLICKED";
-        chrome.runtime.sendMessage({wm: "closeEverything"});
-    });
-
-    document.getElementById("debug").addEventListener('click', function(){
-        status.textContent = "DEBUG CLICKED";
-        chrome.runtime.sendMessage({wm: "showInternalVariables"});
-    });
-
-    document.getElementById("config").addEventListener('click', function(){
-        status.textContent = "CONFIG CLICKED";
-        chrome.windows.getCurrent(false, ["normal"]);
+    document.getElementById("identify").addEventListener('click', function(){
+       status.textContent = "IDENTIFY CLICKED";
+       chrome.runtime.sendMessage({wm: "identifyDisplays"});
     });
 });
