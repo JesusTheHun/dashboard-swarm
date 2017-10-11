4d88934b7d6bc0b01aac9b7f778fda9da09dacad
Jonathan MASSUCHETTI <jonathan.massuchetti@boursorama.fr>
2017-10-11 18:07:03 +0200

    save config reboot rotation with new value

--- chrome-extension/src/config.js
+++ chrome-extension/src/config.js
@@ -2,18 +2,33 @@ import DashboardSwarmNode from './classes/DashboardSwarmNode';
 
 // Saves options to chrome.storage.sync.
 function save_options() {
-    let data = {
-        master: document.getElementById('master').checked,
-        server: document.getElementById('server').value,
-        interval: document.getElementById('interval').value
-    };
 
-    DashboardSwarmNode.setMaster(data.master);
+    chrome.storage.sync.get({
+        master: false,
+        server: 'localhost:8080',
+        interval: 5000
+    }, function(previousConfig) {
 
-    chrome.storage.sync.set(data, () => {
-        let status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(() => status.textContent = '', 750);
+        let data = {
+            master: document.getElementById('master').checked,
+            server: document.getElementById('server').value,
+            interval: document.getElementById('interval').value
+        };
+
+        DashboardSwarmNode.setMaster(data.master);
+
+        chrome.storage.sync.set(data, () => {
+            let status = document.getElementById('status');
+
+            // Reboot rotation to use new interval
+            if (previousConfig.interval !== data.interval) {
+                chrome.runtime.sendMessage({node: "stopRotation", args: []});
+                chrome.runtime.sendMessage({node: "startRotation", args: [data.interval]});
+            }
+
+            status.textContent = 'Options saved.';
+            setTimeout(() => status.textContent = '', 750);
+        });
     });
 }
 
